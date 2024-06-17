f43bb3c416c9230389cd53bee269cbaa11071085
Tina <tina.chen@u-ark.com>
2024-06-17 15:32:58 +0800

    feat: tab title

---
+++ js/changeTitle.js
@@ -0,0 +1,7 @@
+document.getElementById('basic-tabs-item-1').addEventListener('click', function() {
+    document.getElementById('title').textContent = '新個案初評';
+  });
+
+  document.getElementById('basic-tabs-item-2').addEventListener('click', function() {
+    document.getElementById('title').textContent = '歷史個案複檢';
+  });
