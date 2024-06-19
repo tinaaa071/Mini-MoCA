5d10a705e2c1bca52afb93f8231fb0549c5838d7
Tina <tina.chen@u-ark.com>
2024-06-19 13:03:26 +0800

    doc: change img

--- js/hint.js
+++ js/hint.js
@@ -14,11 +14,11 @@ toggleButton.addEventListener('click', function() {
     toggleText.textContent = '提示二';
   } else if (clickCount === 2) {
     // Second click: Change to the second image
-    imageToShow.src = 'https://images.unsplash.com/photo-1718480419942-a21efc9b5409?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Change to the path of your second image
+    imageToShow.src = '../img/clock2.png'; // Change to the path of your second image
     toggleText.textContent = '提示三';
   } else if (clickCount === 3) {
     // Third click: Change to the third image and hide the button
-    imageToShow.src = 'https://plus.unsplash.com/premium_photo-1671253984802-eaa9aa4adbf5?q=80&w=2750&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // Change to the path of your third image
+    imageToShow.src = '../img/clock-g.png'; // Change to the path of your third image
     toggleButton.classList.add('hidden');
     imageCaption.classList.remove('hidden');
   }
