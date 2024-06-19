0d7c266b781e479de40fdbc13e1782dbae184616
Tina <tina.chen@u-ark.com>
2024-06-19 14:32:31 +0800

    fix: tab

--- js/edit.js
+++ js/edit.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('edit').addEventListener('click', edit);
   document.getElementById('cancel').addEventListener('click', cancelEdit);
+
+  document.getElementById('basic-tabs-item-1').addEventListener('click', activateTab1);
+  document.getElementById('basic-tabs-item-2').addEventListener('click', activateTab2);
 });
 
 function edit() {
@@ -13,12 +16,7 @@ function edit() {
   document.getElementById('nextPage').classList.add('hidden');
 
   // Activate basic-tabs-item-1
-  document.getElementById('basic-tabs-item-1').classList.add('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
-  document.getElementById('basic-tabs-item-1').classList.remove('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
-
-  // Deactivate basic-tabs-item-2
-  document.getElementById('basic-tabs-item-2').classList.remove('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
-  document.getElementById('basic-tabs-item-2').classList.add('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
+  activateTab1();
 
   // Change title
   document.getElementById('title').textContent = '舊個案複檢';
@@ -34,13 +32,38 @@ function cancelEdit() {
   document.getElementById('nextPage').classList.remove('hidden');
 
   // Activate basic-tabs-item-2
-  document.getElementById('basic-tabs-item-2').classList.add('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
-  document.getElementById('basic-tabs-item-2').classList.remove('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
-
-  // Deactivate basic-tabs-item-1
-  document.getElementById('basic-tabs-item-1').classList.remove('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
-  document.getElementById('basic-tabs-item-1').classList.add('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
+  activateTab2();
 
   // Change title
   document.getElementById('title').textContent = '歷史個案複檢';
+}
+
+function activateTab1() {
+  // Activate Tab 1
+  document.getElementById('basic-tabs-item-1').classList.add('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
+  document.getElementById('basic-tabs-item-1').classList.remove('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
+
+  // Deactivate Tab 2
+  document.getElementById('basic-tabs-item-2').classList.remove('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
+  document.getElementById('basic-tabs-item-2').classList.add('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
+
+  // Show content for Tab 1
+  document.getElementById('basic-tabs-1').classList.remove('hidden');
+  // Hide content for Tab 2
+  document.getElementById('basic-tabs-2').classList.add('hidden');
+}
+
+function activateTab2() {
+  // Activate Tab 2
+  document.getElementById('basic-tabs-item-2').classList.add('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
+  document.getElementById('basic-tabs-item-2').classList.remove('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
+
+  // Deactivate Tab 1
+  document.getElementById('basic-tabs-item-1').classList.remove('active', 'font-semibold', 'border-amber-300', 'text-gray-900');
+  document.getElementById('basic-tabs-item-1').classList.add('text-gray-500', 'border-transparent', 'hover:text-amber-300', 'disabled:opacity-50', 'disabled:pointer-events-none');
+
+  // Show content for Tab 2
+  document.getElementById('basic-tabs-2').classList.remove('hidden');
+  // Hide content for Tab 1
+  document.getElementById('basic-tabs-1').classList.add('hidden');
 }
--- js/tabContent.js
+++ js/tabContent.js
@@ -1,143 +1,178 @@
-document.querySelector('.change-content-button').addEventListener('click', function() {
-    // Get the content element within basic-tabs-2
-    const tabContentElement = document.getElementById('tab-content2');
-    
-    // Create a new div element
-    const newDiv = document.createElement('div');
-    newDiv.className = 'new-content-div'; // Add any classes you want
-    newDiv.innerHTML = `
-      <div class="grid grid-cols-12 gap-6">
-        <!-- 左 -->
-        <div class="col-span-8 p-8 mb-12 rounded-2xl bg-blue-50">
-          <!-- 表頭 -->
-          <div class="flex justify-between pb-4 mb-4 text-2xl border-b-4 border-gray-300">
-            <p class="text-gray-500">請填寫個案基本資訊</p>
-            <label for="quantity" class="text-gray-700">案號:18</label>
+// Variable to store the original content of tab-content2
+let originalTabContent;
+
+// Function to add event listeners to change-content-button buttons
+function addEventListenersToButtons() {
+  document.querySelectorAll('.change-content-button').forEach(button => {
+    button.addEventListener('click', function() {
+      // Remove the active class from all buttons
+      document.querySelectorAll('.change-content-button').forEach(btn => btn.classList.remove('active'));
+
+      
+      // Get the content element within basic-tabs-2
+      const tabContentElement = document.getElementById('tab-content2');
+      
+      // If originalTabContent is not yet set, store the current content of tab-content2
+      if (!originalTabContent) {
+        originalTabContent = tabContentElement.innerHTML;
+      }
+      
+      // Create a new div element
+      const newDiv = document.createElement('div');
+      newDiv.className = 'new-content-div'; // Add any classes you want
+      newDiv.innerHTML = `
+        <div class="h-full flex flex-col justify-start">
+          <button type="button" id="back" class="text-2xl inline-flex items-center mb-6">
+            <i class="fa-solid fa-arrow-left-long mr-2"></i>
+            回前頁
+          </button>
+          <div class="grid grid-cols-12 gap-6">
+            <!-- Left -->
+            <div class="col-span-8 p-8 mb-12 rounded-2xl bg-blue-50">
+              <!-- Header -->
+              <div class="flex justify-between pb-4 mb-4 text-2xl border-b-4 border-gray-300">
+                <p class="text-gray-500">請填寫個案基本資訊</p>
+                <label for="quantity" class="text-gray-700">案號:18</label>
+              </div>
+              <!-- Fields -->
+              <div class="flex flex-col gap-6 text-2xl text-gray-500">
+                <!-- Name -->
+                <div class="flex items-center gap-8">
+                  <label for="input-label" class="font-medium whitespace-nowrap">姓名</label>
+                  <p>陳小美</p>
+                </div>
+                <!-- Age -->
+                <div class="flex items-center gap-8">
+                  <label for="input-label" class="block mb-2 font-medium whitespace-nowrap">年齡</label>
+                  <p>87</p>
+                </div>
+                <!-- Gender -->
+                <div class="flex items-center gap-8">
+                  <label for="input-label" class="block mb-2 font-medium whitespace-nowrap">性別</label>
+                  <p>女</p>
+                </div>
+                <!-- Education -->
+                <div class="flex items-center gap-8">
+                  <label for="input-label" class="block mb-2 font-medium whitespace-nowrap">教育</label>
+                  <p>未受教育</p>
+                </div>
+              </div>
+            </div>
+            <!-- Right -->
+            <div class="col-span-4 p-8 mb-12 rounded-2xl bg-blue-50">
+              <!-- Header -->
+              <div class="pb-4 text-2xl border-b-4 border-gray-300">
+                <p class="text-center text-gray-500 ">篩檢紀錄</p>
+              </div>
+              <!-- Fields -->
+              <div class="flex flex-col overflow-y-scroll text-2xl text-left text-gray-500 h-96">
+                <!-- 個案 -->
+            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
+              <div class="flex items-center justify-between mb-4 ">
+                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
+                  陳小美
+                </label>
+                <p class="text-blue-600 ">
+                  17 分
+                </p>
+              </div>
+              <p class="text-lg">
+                測驗時間：
+                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
+                </time>
+              </p>
+              <p class="text-lg">
+                花費時間：
+                <time>04:49:12</time>
+              </p>
+            </a>
+            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
+              <div class="flex items-center justify-between mb-4 ">
+                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
+                  陳小美
+                </label>
+                <p class="text-blue-600 ">
+                  17 分
+                </p>
+              </div>
+              <p class="text-lg">
+                測驗時間：
+                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
+                </time>
+              </p>
+              <p class="text-lg">
+                花費時間：
+                <time>04:49:12</time>
+              </p>
+            </a>
+            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
+              <div class="flex items-center justify-between mb-4 ">
+                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
+                  陳小美
+                </label>
+                <p class="text-blue-600 ">
+                  17 分
+                </p>
+              </div>
+              <p class="text-lg">
+                測驗時間：
+                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
+                </time>
+              </p>
+              <p class="text-lg">
+                花費時間：
+                <time>04:49:12</time>
+              </p>
+            </a>
+            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
+              <div class="flex items-center justify-between mb-4 ">
+                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
+                  陳小美
+                </label>
+                <p class="text-blue-600 ">
+                  17 分
+                </p>
+              </div>
+              <p class="text-lg">
+                測驗時間：
+                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
+                </time>
+              </p>
+              <p class="text-lg">
+                花費時間：
+                <time>04:49:12</time>
+              </p>
+            </a>
+              </div>
+            </div>
           </div>
-          <!-- 欄位 -->
-          <div class="flex flex-col gap-6 text-2xl text-gray-500">
-            <!-- 姓名 -->
-            <div class="flex items-center gap-8">
-              <label for="input-label" class="font-medium whitespace-nowrap">姓名</label>
-              <p>陳小美</p>
-            </div>
-            <!-- 年齡 -->
-            <div class="flex items-center gap-8">
-              <label for="input-label" class="block mb-2 font-medium whitespace-nowrap">年齡</label>
-              <p>87</p>
-            </div>
-            <!-- 性別 -->
-            <div class="flex items-center gap-8">
-              <label for="input-label" class="block mb-2 font-medium whitespace-nowrap">性別</label>
-              <p>女</p>
-            </div>
-            <!-- 教育 -->
-            <div class="flex items-center gap-8">
-              <label for="input-label" class="block mb-2 font-medium whitespace-nowrap">教育</label>
-              <p>未受教育</p>
-            </div>
+          <!-- Buttons -->
+          <div class="flex justify-center gap-6">
+            <button id="edit" onclick="edit()" class="px-12 py-5 text-3xl font-bold text-gray-600 bg-white border border-gray-600 rounded-full w-fit">
+              編輯個案資訊
+            </button>
+            <a href="個案測前觀察.html" class="px-12 py-5 text-3xl font-bold text-gray-900 rounded-full bg-amber-300 w-fit">
+              下一步
+            </a>
           </div>
         </div>
-        <!-- 右 -->
-        <div class="col-span-4 p-8 mb-12 rounded-2xl bg-blue-50">
-          <!-- 表頭 -->
-          <div class="pb-4 text-2xl border-b-4 border-gray-300">
-            <p class="text-center text-gray-500 ">篩檢紀錄</p>
-          </div>
-          <!-- 欄位 -->
-          <div class="flex flex-col overflow-y-scroll text-2xl text-left text-gray-500">
-            <!-- 個案 -->
-            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
-              <div class="flex items-center justify-between mb-4 ">
-                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
-                  陳小美
-                </label>
-                <p class="text-blue-600 ">
-                  17 分
-                </p>
-              </div>
-              <p class="text-lg">
-                測驗時間：
-                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
-                </time>
-              </p>
-              <p class="text-lg">
-                花費時間：
-                <time>04:49:12</time>
-              </p>
-            </a>
-            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
-              <div class="flex items-center justify-between mb-4 ">
-                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
-                  陳小美
-                </label>
-                <p class="text-blue-600 ">
-                  17 分
-                </p>
-              </div>
-              <p class="text-lg">
-                測驗時間：
-                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
-                </time>
-              </p>
-              <p class="text-lg">
-                花費時間：
-                <time>04:49:12</time>
-              </p>
-            </a>
-            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
-              <div class="flex items-center justify-between mb-4 ">
-                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
-                  陳小美
-                </label>
-                <p class="text-blue-600 ">
-                  17 分
-                </p>
-              </div>
-              <p class="text-lg">
-                測驗時間：
-                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
-                </time>
-              </p>
-              <p class="text-lg">
-                花費時間：
-                <time>04:49:12</time>
-              </p>
-            </a>
-            <a href="篩檢紀錄.html" class="px-2 py-4 border-b-2 border-gray-300 border-dashed rounded-lg hover:bg-amber-300 active:bg-amber-300">
-              <div class="flex items-center justify-between mb-4 ">
-                <label for="input-label" class="font-medium text-gray-900 whitespace-nowrap ">
-                  陳小美
-                </label>
-                <p class="text-blue-600 ">
-                  17 分
-                </p>
-              </div>
-              <p class="text-lg">
-                測驗時間：
-                <time datetime="2024-07-02 T17:25">2024/07/02 17:25
-                </time>
-              </p>
-              <p class="text-lg">
-                花費時間：
-                <time>04:49:12</time>
-              </p>
-            </a>
-          </div>
-        </div>
-      </div>
-      <!-- 按鈕 -->
-      <div class="flex justify-center gap-6">
-        <button id="edit" onclick="edit()" class="px-12 py-5 text-3xl font-bold text-gray-600 bg-white border border-gray-600 rounded-full w-fit">
-          編輯個案資訊
-        </button>
-        <a href="個案測前觀察.html" class="px-12 py-5 text-3xl font-bold text-gray-900 rounded-full bg-amber-300 w-fit">
-          下一步
-        </a>
-    </div>
-    `;
-    
-    // Replace the existing content with the new div
-    tabContentElement.innerHTML = ''; // Clear the existing content
-    tabContentElement.appendChild(newDiv); // Append the new div
+      `;
+      
+      // Replace the existing content with the new div
+      tabContentElement.innerHTML = ''; // Clear the existing content
+      tabContentElement.appendChild(newDiv); // Append the new div
+      
+      // Event listener for the back button to revert to the original content
+      document.getElementById('back').addEventListener('click', function() {
+        tabContentElement.innerHTML = originalTabContent; // Revert to original content
+        originalTabContent = null; // Reset originalTabContent
+        
+        // Reattach event listeners to change-content-button buttons
+        addEventListenersToButtons();
+      });
+    });
   });
+}
+
+// Initially add event listeners to change-content-button buttons
+addEventListenersToButtons();
