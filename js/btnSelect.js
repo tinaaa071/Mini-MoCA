0467572c6c32f8bf12a5f06600839ff229ba7ab6
Tina <tina.chen@u-ark.com>
2024-07-10 16:33:06 +0800

    style: btn

--- js/btnSelect.js
+++ js/btnSelect.js
@@ -40,4 +40,6 @@ function addButtonGroupClickListener(groupClass, buttonClass) {
 addButtonGroupClickListener('.gender-group', '.gender-btn');
 addButtonGroupClickListener('.education-group', '.education-btn');
 });
+
+
   
